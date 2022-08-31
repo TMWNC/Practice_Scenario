5db0b418edfe5601f3e9f5b0eb64337c5273fcbd
TMWNC <79842944+TMWNC@users.noreply.github.com>
2022-08-31 08:47:56 +0100

    Add files via upload

---
+++ frmInput.cs
@@ -0,0 +1,72 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSV_KPI
+{
+    public partial class frmInput : Form
+    {
+        public frmInput()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+            addRecord(int.Parse(tbID.Text), tbName.Text, int.Parse(tbCompletion.Text));
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            bool myCondition = true;
+
+           
+            if (MessageBox.Show("You are about to clear your form, Do you wish to continue?", "Confirm?", MessageBoxButtons.YesNo, MessageBoxIcon.Question, myCondition ? MessageBoxDefaultButton.Button2 : MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                tbID.Text = "";
+                tbName.Text = "";
+                tbCompletion.Text = "";
+            }
+        }
+
+        public static void addRecord(int iID, string sName, int iCompletion)
+        {
+            
+            try
+            {
+                
+                using (StreamWriter file = new StreamWriter("StudentCompletion.csv", true))
+                {
+                   
+                    file.WriteLine($"{iID},{sName},{iCompletion}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("There was an error: ", ex);
+            }
+        }
+
+        private void frmInput_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            frmRetrieve newForm = new frmRetrieve();
+            this.Hide();
+            newForm.Show();
+        }
+    }
+}
---
+++ frmRetrieve.cs
@@ -0,0 +1,76 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Dynamic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSV_KPI
+{
+    public partial class frmRetrieve : Form
+    {
+        public frmRetrieve()
+        {
+            InitializeComponent();
+        }
+      
+        public List<dynamic> records = new List<dynamic>();
+
+        private void btnRetrieveLB_Click(object sender, EventArgs e)
+        {
+            recordList();
+
+            foreach (var record in records) 
+            {
+             
+                lbDisplay.Items.Add($"{record.ID}, {record.Name}, {record.Completion}");
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+          
+            int recordSelect = (int.Parse(cbChoice.Text)-1);
+
+            recordList();
+            lblNameout.Text = records.ElementAt(recordSelect).Name;
+            lblCompout.Text = records.ElementAt(recordSelect).Completion;
+            pbComp.Value = int.Parse(records.ElementAt(recordSelect).Completion);
+        }
+
+        private void frmRetrieve_Load(object sender, EventArgs e)
+        {
+            recordList();
+            foreach (var record in records)
+            {
+                cbChoice.Items.Add($"{record.ID}");
+            }
+        }
+
+        public void recordList()
+        {
+            try
+            {
+               
+                using (var fileRead = new StreamReader("StudentCompletion.csv"))
+
+                using (var csv = new CsvReader(fileRead, CultureInfo.InvariantCulture))
+                {
+                 
+                    records = csv.GetRecords<dynamic>().ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("There was an error: ", ex);
+            }
+        }
+    }
+}
